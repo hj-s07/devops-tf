c84db870e754fd2650b64f6ba44c584b7d17e06c
devops <devops@dev.ops>
2025-01-03 16:47:26 +0900

    0103 terraform

--- phpdemo/user-data.sh
+++ phpdemo/user-data.sh
@@ -6,12 +6,13 @@ export DB_URL=${db_url}
 sed -i "s/DB_IP/$DB_URL/g" cloud-demo/process_create.php
 
 export DB_USER=${db_username}
-sed -i "s/root/$DB_USER/g" cloud-demo/process_create.php
+sed -i "s/USERNAMEt/$DB_USER/g" cloud-demo/process_create.php
 
 export DB_PASS=${db_password}
-sed -i "s/Test123!/$DB_PASS/g" cloud-demo/process_create.php
+sed -i "s/PASSWORD/$DB_PASS/g" cloud-demo/process_create.php
 
-cp cloud-demo/*.php /var/www/html
+export DB_NAME=${db_name}
+sed -i "s/NAME/$DB_NAME/g" cloud-demo/process_create.php
 
 # 웹 서버 재시작
 sudo systemctl restart httpd
